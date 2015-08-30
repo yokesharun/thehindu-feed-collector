0becd2121b84f3e3b6340d8dbc0f1d08d20f9a19
yokesh <yokesh@yokeshs-MacBook-Air.local>
2015-08-30 16:05:07 +0530

    initiated the material design

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
 <head>
 	<title>The Hindu Feeder</title>
+
+	  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+  
 </head>
 <body>
 
